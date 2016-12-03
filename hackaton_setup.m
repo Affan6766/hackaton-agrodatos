258393341f1cf11c6950480ebe7afe36211cc536
José Ignacio Orlando <ignaciorlando@gmail.com>
2016-12-03 20:57:57 -0300

    Plot movement debugged + heat map video generation

--- hackaton_setup.m
+++ hackaton_setup.m
@@ -8,6 +8,7 @@ addpath(genpath(fullfile(root, 'matconvnet')));
 addpath(genpath(fullfile(root, 'core')));
 addpath(genpath(fullfile(root, 'demos')));
 addpath(genpath(fullfile(root, 'lib')));
+addpath(genpath(fullfile(root, 'experiments')));
 
 % compile matconvnet if necessary
 if numel(dir(fullfile(root, 'matconvnet', 'matlab', 'mex', 'vl_nnconv.mex*'))) == 0
