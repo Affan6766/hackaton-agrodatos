96058bdccee16b81bd5b66b7a6630e766cbc580f
José Ignacio Orlando <ignaciorlando@gmail.com>
2016-12-04 10:47:42 -0300

    Plot movement now considers also precomputed mat files

--- experiments/plot_movement.m
+++ experiments/plot_movement.m
@@ -21,9 +21,15 @@ frames = parse_video(video_filename, begin_parse, end_parse);
 % convert each frame to grayscale and normalize gray intensities
 fprintf('Converting each frame to grayscale and normalizing...\n');
 frames_grayscale = preprocess_frames_for_movement_detection(frames);
-% get foreground from each frame sequence
-fprintf('Getting foregrounds from each frame...\n');
-foregrounds_batch = get_foregrounds_from_frames_sequences(frames, net, opts);
+% segment out the foreground
+fprintf('Getting foreground from each frame...(%d frames)\n', size(frames,4));
+foregrounds_filename = strcat(video_filename,'_',num2str(start_time),'-',num2str(end_time),'_foreground.mat');
+if exist(foregrounds_filename,'file') == 0
+    foregrounds_batch = get_foregrounds_from_frames_sequences(frames, net, opts);
+    save(foregrounds_filename, 'foregrounds_batch');
+else
+    load(foregrounds_filename);
+end
 
 %% compute movement score
 
